2e9505eb016f288a2d39fb8db1034ee37a5da230
xDark <me@xdark.dev>
2023-04-22 02:01:20 +0300

    Unify functions required to unlock the flag for future Linux version (?)

--- src/main/java/dev/xdark/unsolver/Unsolver.java
+++ src/main/java/dev/xdark/unsolver/Unsolver.java
@@ -1,172 +1,115 @@
 package dev.xdark.unsolver;
 
 import com.sun.jna.Memory;
-import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.Psapi;
-import com.sun.jna.platform.win32.WinDef;
-import com.sun.jna.platform.win32.WinNT;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.win32.StdCallLibrary;
-import com.sun.jna.win32.W32APIOptions;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Unsolver {
+public final class Unsolver {
+	private Unsolver() {
+	}
 
 	public static void enableDynamicAgentLoading(long pid) {
-		WinNT.HANDLE targetProcess = checkNotNull(Kernel32.INSTANCE.OpenProcess(
-				0x0008 | 0x0010 | 0x0020,
-				false,
-				(int) pid
-		), "OpenProcess");
+		ProcessAccess access = new WindowsProcessAccess();
+		Object targetProcess = access.openProcess(pid);
 		try {
-			WinNT.HANDLE thisProcess = Kernel32.INSTANCE.GetCurrentProcess();
-			IntByReference bytesRead = new IntByReference();
-			IntByReference ref = bytesRead;
-			checkTrue(Kernel32.INSTANCE.IsWow64Process(thisProcess, ref), "IsWow64Process this");
-			int pointerSize = ref.getValue() == 1 ? 4 : 8;
-			checkTrue(Kernel32.INSTANCE.IsWow64Process(targetProcess, ref), "IsWow64Process target");
-			boolean isTargetProcess64bit = ref.getValue() == 0;
-			int targetSize = isTargetProcess64bit ? 8 : 4;
-			WinDef.HMODULE[] modules = new WinDef.HMODULE[512];
-			checkTrue(Psapi.INSTANCE.EnumProcessModules(
-					targetProcess,
-					modules,
-					512 * pointerSize,
-					ref
-			), "EnumProcessModules");
-			char[] pathBuf = new char[260];
-			for (int i = 0, j = ref.getValue() / pointerSize; i < j; i++) {
-				WinDef.HMODULE moduleHandle = modules[i];
-				int len = Psapi.INSTANCE.GetModuleFileNameExW(targetProcess, moduleHandle, pathBuf, 260);
-				checkTrue(len != 0, "GetModuleFileNameExA");
-				String moduleName = new String(pathBuf, 0, len);
-				if (moduleName.contains("jvm.dll")) {
-					WinSymbolLookup lookup = new WinSymbolLookup(moduleHandle, isTargetProcess64bit);
+			Object jvm = Util.checkNotNull(access.findLibJvm(targetProcess), "libjvm");
+			boolean is64bit = access.is64Bit(targetProcess);
+			int targetSize = is64bit ? 8 : 4;
+			SymbolLookup lookup = new SymbolLookup(access, jvm, is64bit);
 
-					Map<String, Map<String, Field>> map = new HashMap<>();
-					{
-						long entry = lookup.getSymbol("gHotSpotVMStructs");
-						long typeNameOffset = lookup.getSymbol("gHotSpotVMStructEntryTypeNameOffset");
-						long fieldNameOffset = lookup.getSymbol("gHotSpotVMStructEntryFieldNameOffset");
-						long stride = lookup.getSymbol("gHotSpotVMStructEntryArrayStride");
-						long isStaticOffset = lookup.getSymbol("gHotSpotVMStructEntryIsStaticOffset");
-						long addressOffset = lookup.getSymbol("gHotSpotVMStructEntryAddressOffset");
-						long offsetOffset = lookup.getSymbol("gHotSpotVMStructEntryOffsetOffset");
-						WinHotSpotTraverse traverse = new WinHotSpotTraverse(
-								targetProcess,
-								targetSize,
-								typeNameOffset,
-								fieldNameOffset,
-								isStaticOffset,
-								addressOffset,
-								offsetOffset
-						);
-						while (true) {
-							String fieldName = traverse.getFieldName(entry);
-							if (fieldName == null) {
-								break;
+			Map<String, Map<String, Field>> map = new HashMap<>();
+			{
+				long entry = lookup.getSymbol("gHotSpotVMStructs");
+				long typeNameOffset = lookup.getSymbol("gHotSpotVMStructEntryTypeNameOffset");
+				long fieldNameOffset = lookup.getSymbol("gHotSpotVMStructEntryFieldNameOffset");
+				long stride = lookup.getSymbol("gHotSpotVMStructEntryArrayStride");
+				long isStaticOffset = lookup.getSymbol("gHotSpotVMStructEntryIsStaticOffset");
+				long addressOffset = lookup.getSymbol("gHotSpotVMStructEntryAddressOffset");
+				long offsetOffset = lookup.getSymbol("gHotSpotVMStructEntryOffsetOffset");
+				HotSpotTraverse traverse = new HotSpotTraverse(
+						access,
+						targetProcess,
+						targetSize,
+						typeNameOffset,
+						fieldNameOffset,
+						isStaticOffset,
+						addressOffset,
+						offsetOffset
+				);
+				while (true) {
+					String fieldName = traverse.getFieldName(entry);
+					if (fieldName == null) {
+						break;
+					}
+					String typeName = traverse.getTypeName(entry);
+					boolean isStatic = traverse.isStatic(entry);
+					long offset = isStatic ? traverse.getAddress(entry) : traverse.getOffset(entry);
+					map.computeIfAbsent(typeName, __ -> new HashMap<>())
+							.put(fieldName, new Field(offset, isStatic));
+					entry += stride;
+				}
+			}
+			String flagTypeName = "Flag";
+			Map<String, Field> type = map.get(flagTypeName);
+			if (type == null) {
+				flagTypeName = "JVMFlag";
+				type = map.get(flagTypeName);
+			}
+			Util.checkNotNull(type, "no JVMFlag in target process");
+			{
+				long entry = lookup.getSymbol("gHotSpotVMTypes");
+				long typeNameOffset = lookup.getSymbol("gHotSpotVMTypeEntryTypeNameOffset");
+				long sizeOffset = lookup.getSymbol("gHotSpotVMTypeEntrySizeOffset");
+				long stride = lookup.getSymbol("gHotSpotVMTypeEntryArrayStride");
+				HotSpotTraverse traverse = new HotSpotTraverse(
+						access,
+						targetProcess,
+						targetSize,
+						typeNameOffset,
+						0L,
+						0L,
+						0L,
+						sizeOffset
+				);
+				while (true) {
+					String typeName = traverse.getTypeName(entry);
+					if (typeName == null) {
+						break;
+					}
+					if (flagTypeName.equals(typeName)) {
+						int size = traverse.getOffset(entry);
+						long numFlags = type.get("numFlags").offset;
+						Memory memory = new Memory(1024);
+						access.readProcessMemory(targetProcess, Pointer.createConstant(numFlags), memory, 4);
+						int flagCount = memory.getInt(0);
+						access.readProcessMemory(targetProcess, Pointer.createConstant(type.get("flags").offset), memory, targetSize);
+						Pointer baseFlagAddress = Util.checkNotNull(readPointer(memory, targetSize), "base flag pointer");
+						long _name = type.get("_name").offset;
+						long _addr = type.get("_addr").offset;
+
+						for (int k = 0; k < flagCount - 1; k++) {
+							Pointer flagAddress = baseFlagAddress.share((long) k * size);
+							int bytesRead = access.readProcessMemory(targetProcess, flagAddress.share(_name), memory, 1024);
+							String flagName = readStringA(readPointer(memory, targetSize), bytesRead);
+							if ("EnableDynamicAgentLoading".equals(flagName)) {
+								access.readProcessMemory(targetProcess, flagAddress.share(_addr), memory, targetSize);
+								Pointer valueAddress = readPointer(memory, targetSize);
+								memory.setByte(0L, (byte) 1);
+								access.writeProcessMemory(targetProcess, valueAddress, memory, 1);
+								return;
 							}
-							String typeName = traverse.getTypeName(entry);
-							boolean isStatic = traverse.isStatic(entry);
-							long offset = isStatic ? traverse.getAddress(entry) : traverse.getOffset(entry);
-							map.computeIfAbsent(typeName, __ -> new HashMap<>())
-									.put(fieldName, new Field(offset, isStatic));
-							entry += stride;
 						}
 					}
-					String flagTypeName = "Flag";
-					Map<String, Field> type = map.get(flagTypeName);
-					if (type == null) {
-						flagTypeName = "JVMFlag";
-						type = map.get(flagTypeName);
-					}
-					checkNotNull(type, "no JVMFlag in target process");
-					{
-						long entry = lookup.getSymbol("gHotSpotVMTypes");
-						long typeNameOffset = lookup.getSymbol("gHotSpotVMTypeEntryTypeNameOffset");
-						long sizeOffset = lookup.getSymbol("gHotSpotVMTypeEntrySizeOffset");
-						long stride = lookup.getSymbol("gHotSpotVMTypeEntryArrayStride");
-						WinHotSpotTraverse traverse = new WinHotSpotTraverse(
-								targetProcess,
-								targetSize,
-								typeNameOffset,
-								0L,
-								0L,
-								0L,
-								sizeOffset
-						);
-						while (true) {
-							String typeName = traverse.getTypeName(entry);
-							if (typeName == null) {
-								break;
-							}
-							if (flagTypeName.equals(typeName)) {
-								int size = traverse.getOffset(entry);
-								long numFlags = type.get("numFlags").offset;
-								Memory memory = new Memory(1024);
-								checkTrue(
-										Kernel32.INSTANCE.ReadProcessMemory(targetProcess, Pointer.createConstant(numFlags), memory, 4, bytesRead),
-										"ReadProcessMemory numFlags"
-								);
-								int flagCount = memory.getInt(0);
-								checkTrue(
-										Kernel32.INSTANCE.ReadProcessMemory(targetProcess, Pointer.createConstant(type.get("flags").offset), memory, targetSize, bytesRead),
-										"ReadProcessMemory flags"
-								);
-								Pointer baseFlagAddress = checkNotNull(readPointer(memory, targetSize), "base flag pointer");
-								long _name = type.get("_name").offset;
-								long _addr = type.get("_addr").offset;
-
-								for (int k = 0; k < flagCount - 1; k++) {
-									Pointer flagAddress = baseFlagAddress.share((long) k * size);
-									checkTrue(
-											Kernel32.INSTANCE.ReadProcessMemory(targetProcess, flagAddress.share(_name), memory, 1024, bytesRead),
-											"ReadProcessMemory name"
-									);
-									String flagName = readStringA(readPointer(memory, targetSize), bytesRead.getValue());
-									if ("EnableDynamicAgentLoading".equals(flagName)) {
-										checkTrue(
-												Kernel32.INSTANCE.ReadProcessMemory(targetProcess, flagAddress.share(_addr), memory, targetSize, bytesRead),
-												"ReadProcessMemory address"
-										);
-										Pointer valueAddress = readPointer(memory, targetSize);
-										memory.setByte(0L, (byte) 1);
-										checkTrue(
-												Kernel32.INSTANCE.WriteProcessMemory(targetProcess, valueAddress, memory, 1, bytesRead),
-												"WriteProcessMemory"
-										);
-										return;
-									}
-								}
-							}
-							entry += stride;
-						}
-					}
-					break;
+					entry += stride;
 				}
 			}
 		} finally {
-			Kernel32.INSTANCE.CloseHandle(targetProcess);
+			access.closeProcess(targetProcess);
 		}
 		throw new IllegalStateException("Could not change EnableDynamicAgentLoading flag");
-	}
-
-	static <T> T checkNotNull(T any, String msg) {
-		if (any == null) {
-			throw new IllegalStateException(msg);
-		}
-		return any;
-	}
-
-	static void checkTrue(boolean condition, String msg) {
-		if (!condition) {
-			throw new IllegalStateException(msg);
-		}
 	}
 
 	private static Pointer readPointer(Pointer from, int ptrSize) {
@@ -191,10 +134,6 @@ public class Unsolver {
 		return new String(buf, 0, i);
 	}
 
-	private static String readStringA(Pointer pointer, IntByReference size) {
-		return readStringA(pointer, size.getValue());
-	}
-
 	private static final class Field {
 		final long offset;
 		final boolean isStatic;
@@ -205,8 +144,9 @@ public class Unsolver {
 		}
 	}
 
-	private static final class WinHotSpotTraverse {
-		final WinNT.HANDLE process;
+	private static final class HotSpotTraverse {
+		final ProcessAccess access;
+		final Object process;
 		final int pointerSize;
 		final long typeNameOffset;
 		final long fieldNameOffset;
@@ -215,7 +155,8 @@ public class Unsolver {
 		final long offsetOffset;
 		final Memory memory;
 
-		WinHotSpotTraverse(WinNT.HANDLE process, int pointerSize, long typeNameOffset, long fieldNameOffset, long isStaticOffset, long addressOffset, long offsetOffset) {
+		HotSpotTraverse(ProcessAccess access, Object process, int pointerSize, long typeNameOffset, long fieldNameOffset, long isStaticOffset, long addressOffset, long offsetOffset) {
+			this.access = access;
 			this.process = process;
 			this.pointerSize = pointerSize;
 			this.typeNameOffset = typeNameOffset;
@@ -235,74 +176,46 @@ public class Unsolver {
 		}
 
 		boolean isStatic(long entry) {
-			IntByReference bytesRead = new IntByReference();
-			checkTrue(
-					Kernel32.INSTANCE.ReadProcessMemory(process, Pointer.createConstant(entry + isStaticOffset), memory, 4, bytesRead),
-					"ReadProcessMemory address"
-			);
+			access.readProcessMemory(process, Pointer.createConstant(entry + isStaticOffset), memory, 4);
 			return memory.getInt(0) == 1;
 		}
 
 		int getOffset(long entry) {
-			IntByReference bytesRead = new IntByReference();
-			checkTrue(
-					Kernel32.INSTANCE.ReadProcessMemory(process, Pointer.createConstant(entry + offsetOffset), memory, 4, bytesRead),
-					"ReadProcessMemory address"
-			);
+			access.readProcessMemory(process, Pointer.createConstant(entry + offsetOffset), memory, 4);
 			return memory.getInt(0);
 		}
 
 		long getAddress(long entry) {
-			IntByReference bytesRead = new IntByReference();
-			checkTrue(
-					Kernel32.INSTANCE.ReadProcessMemory(process, Pointer.createConstant(entry + addressOffset), memory, pointerSize, bytesRead),
-					"ReadProcessMemory address"
-			);
+			access.readProcessMemory(process, Pointer.createConstant(entry + addressOffset), memory, pointerSize);
 			return pointerSize == 8 ? memory.getLong(0) : memory.getInt(0);
 		}
 
 		private String getStringAt(long entry, long offset) {
-			IntByReference bytesRead = new IntByReference();
-			checkTrue(
-					Kernel32.INSTANCE.ReadProcessMemory(process, Pointer.createConstant(entry + offset), memory, pointerSize, bytesRead),
-					"ReadProcessMemory address"
-			);
+			access.readProcessMemory(process, Pointer.createConstant(entry + offset), memory, pointerSize);
 			Pointer ptr = readPointer(memory, pointerSize);
 			if (ptr == null) {
 				return null;
 			}
-			checkTrue(
-					Kernel32.INSTANCE.ReadProcessMemory(process, ptr, memory, 1024, bytesRead),
-					"ReadProcessMemory content"
-			);
+			int bytesRead = access.readProcessMemory(process, ptr, memory, 1024);
 			return readStringA(memory, bytesRead);
 		}
 	}
 
-	private static final class WinSymbolLookup {
+	private static final class SymbolLookup {
 
-		final WinDef.HMODULE module;
+		final ProcessAccess access;
+		final Object module;
 		final boolean is64bit;
 
-		WinSymbolLookup(WinDef.HMODULE module, boolean is64bit) {
+		SymbolLookup(ProcessAccess access, Object module, boolean is64bit) {
+			this.access = access;
 			this.module = module;
 			this.is64bit = is64bit;
 		}
 
 		long getSymbol(String name) {
-			WinDef.LPVOID address = checkNotNull(
-					Kernel32Ext.INSTANCE.GetProcAddress(module, name),
-					name
-			);
-			Pointer ptr = address.getPointer();
+			Pointer ptr = Util.checkNotNull(access.getProcAddress(module, name), name);
 			return is64bit ? ptr.getLong(0) : ptr.getInt(0);
 		}
-	}
-
-	public interface Kernel32Ext extends StdCallLibrary {
-		Kernel32Ext INSTANCE = Native.loadLibrary("kernel32",
-				Kernel32Ext.class, W32APIOptions.ASCII_OPTIONS);
-
-		WinDef.LPVOID GetProcAddress(WinDef.HMODULE hModule, String lpProcName);
 	}
 }
